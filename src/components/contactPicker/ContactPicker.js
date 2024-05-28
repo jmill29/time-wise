826aca5c309d028f207a8092534ad17f3b9b856f
jmill29 <jacoblmiller.jlm@gmail.com>
2024-05-28 17:54:32 -0400

    Add Contact Picker component, enabling users to choose amongst their list of contacts when creating a new appointment

---
+++ src/components/contactPicker/ContactPicker.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import styles from "../../resources/css/ContactPicker.module.css";
+
+export const ContactPicker = ({ value, onChange }) => {
+  const { contacts } = useContext(AppContext);
+  return (
+    <select onChange={onChange} value={value}
+    className={styles.select_input}>
+      <option value=''>Select Contact</option>
+      {contacts.map(el => {
+        if (el.name !== undefined) {
+          return (
+            <option value={String(el.id)}>{el.name}</option>
+          );
+        } else {
+          return (
+            <></>
+          );
+        }
+      })}
+    </select>
+  );
+};
+
